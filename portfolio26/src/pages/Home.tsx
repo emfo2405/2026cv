3085d245de89cb5f143f3a3849aac0fad4699a5d
emfo2405 <emfo2405@student.miun.se>
2026-02-15 18:28:51 +0100

    Informationssektion tillagt på startsida

--- portfolio26/src/pages/Home.tsx
+++ portfolio26/src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import './Home.scss';
+import { Link } from 'react-router-dom';
 
 function Home() {
 
@@ -13,6 +14,22 @@ function Home() {
     <h1 className='headerText'>Emma Forsmalm <br/> Fullstackutvecklare</h1>
     </div>
 
+    <div id='homeAbout'>
+        <div id="aboutContent">
+        <h2>- Om mig -</h2>
+        <p>Hej! <br /> <br /> Jag heter Emma och läser sista året av webbutvecklingsprogrammet vid Mittuniversitetet.
+            Jag är bosatt i Göteborg och läser hela utbildningen på distans. Under programmets gång har jag 
+            fått erfarenhet inom flera olika områden, däribland att bygga fullskaliga webbapplikationer med 
+            olika verktyg och i olika programmeringsspråk. Jag har även läst kurser inriktade på frontend och backend 
+            såväl som fullstack-utveckling. Den här utbildningen har gett mig en bred kompetens samt praktisk 
+            erfarenhet av utveckling med fokus på funktionalitet och användbarhet.
+        </p>
+        <div id='buttonDiv'>
+        <a id="maillink" href="mailto:emma_forsmalm@.com">Kontakta mig!</a>
+        <Link to="/About" id='moreInfo'>Läs mer</Link>
+    </div>
+    </div>
+</div>
 
     <h1>Det här är startsidan!</h1>
     </>
